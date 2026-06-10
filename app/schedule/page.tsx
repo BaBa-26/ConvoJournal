5318509a7f058fc1e9b67d2a04320506890f791c
aarravbalaotech <aarrav.bala@ontariotechu.net>
2026-06-09 23:05:57 -0400

    calender page

---
+++ app/schedule/page.tsx
@@ -0,0 +1,11 @@
+import ScheduleScreen from "@/components/ScheduleScreen";
+import BottomNav from "@/components/BottomNav";
+
+export default function SchedulePage() {
+  return (
+    <>
+      <ScheduleScreen />
+      <BottomNav />
+    </>
+  );
+}
